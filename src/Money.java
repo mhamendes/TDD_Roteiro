0fee2cdef90f72e5926dec12d99e7f2ccc903218
mhamendes <48832695+mhamendes@users.noreply.github.com>
2019-05-19 22:14:53 -0300

    Fourth Commit

--- src/Money.java
+++ src/Money.java
@@ -1,5 +1,6 @@
-class Money  {
+abstract class Money  {
    protected int amount;
+   protected String currency;
 
    public boolean equals(Object object) {
       Money money = (Money) object;
@@ -7,9 +8,35 @@ class Money  {
    }
    abstract Money times(int multiplier);
    static Money dollar(int amount) {
-      return new Dollar(amount);
+      return new Dollar(amount, "USD");
    }
    static Money franc (int amount) {
-     return new Franc(amount);
+     return new Franc(amount, "CHF");
    }
+   String currency() {
+      return currency;
+   }
+   Money(int amount, String currency) {
+     this.amount = amount;
+     this.currency = currency;
+   }
+}
+
+class Dollar extends Money{
+  Dollar(int amount, String currency)  {
+     super(amount, currency);
+  }
+
+  Money times(int multiplier)  {
+     return Money.dollar(amount * multiplier);
+  }
+}
+
+class Franc extends Money{
+  Franc(int amount, String currency) {
+    super(amount, currency);
+  }
+  Money times(int multiplier)  {
+    return Money.franc(amount * multiplier);
+  }
 }
