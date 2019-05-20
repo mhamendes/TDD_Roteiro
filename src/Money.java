988c4a298fc66e35152b0e6845d8e175dde6bbfb
mhamendes <48832695+mhamendes@users.noreply.github.com>
2019-05-19 21:54:46 -0300

    Second Commit

--- src/Money.java
+++ src/Money.java
@@ -1,5 +1,26 @@
-class Dollar {
-   int amount = 10;
-   Dollar(int amount) {}
-   void times(int multiplier) {}
+class Money  {
+   protected int amount;
+
+   public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return amount == money.amount;
+   }
+}
+
+class Dollar extends Money {
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   void times(int multiplier) {
+      amount= amount * multiplier;
+   }
+}
+
+class Franc extends Money{
+   Franc(int amount) {
+      this.amount= amount;
+    }
+    Franc times(int multiplier)  {
+       return new Franc(amount * multiplier);
+    }
 }
